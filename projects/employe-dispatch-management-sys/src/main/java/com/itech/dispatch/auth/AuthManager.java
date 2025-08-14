4540f6bfd724d66af6708200fd254211b52bf8de
Wensheng | Vincent LI <wensheng.li.mit@gmail.com>
2025-08-14 14:19:01 +0900

    Update the methods of verifying the password

--- projects/employe-dispatch-management-sys/src/main/java/com/itech/dispatch/auth/AuthManager.java
+++ projects/employe-dispatch-management-sys/src/main/java/com/itech/dispatch/auth/AuthManager.java
@@ -13,23 +13,54 @@ package com.itech.dispatch.auth;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.itech.dispatch.util.PasswordUtils;
+
 //AuthManager.java
 public class AuthManager {
 	private final Map<String, User> users = new HashMap<>();
 
 	public AuthManager() {
-		// Initialising the test users
-		users.put("admin", new User("admin", "admin123", Role.ADMIN));
-		users.put("manager", new User("manager", "mgr123", Role.MANAGER));
-		users.put("supervisor", new User("supervisor", "sup123", Role.SUPERVISOR));
-		users.put("employee", new User("employee", "emp123", Role.EMPLOYEE));
+		// Initial the test users - char[] password
+		users.put("admin", createUser("admin", "Admin@1234", Role.ADMIN));
+		users.put("manager", createUser("manager", "Manager@5678", Role.MANAGER));
+		users.put("supervisor", createUser("supervisor", "Supervisor@9012", Role.SUPERVISOR));
+		users.put("employee", createUser("employee", "Employee@3456", Role.EMPLOYEE));
 	}
-
-	public User authenticate(String username, String password) {
+	
+	// Create the user
+	private User createUser(String username, String password, Role role) {
+		return new User(username, password.toCharArray(), role);
+	}
+	
+	// Authentication check
+	public User authenticate(String username, char[] password) {
 		User user = users.get(username);
-		if (user != null && user.getPassword().equals(password)) {
+		
+		if (user == null) {
+			return null; // The user is not existing
+		}
+		
+		if (user.isLockOut()) {
+			return null; // The user has been locked out
+		}
+		
+		if (user.verifyPassword(password)) {
 			return user;
 		}
+		
 		return null;
+	}
+	
+	// Add users (The feature of Administrator)
+	public void addUser(String username, char[] password, Role role) {
+		if (users.containsKey(username)) {
+			throw new IllegalArgumentException("The username has been used.");
+		}
+		
+		if (!PasswordUtils.isPasswordStrong(password)) {
+			throw new IllegalArgumentException ("The password is not strong enough.");
+		}
+		
+		users.put(username, new User(username, password, role));
 	}
 }
